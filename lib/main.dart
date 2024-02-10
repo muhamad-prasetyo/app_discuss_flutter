8bd46d6ab2d0812a5bb7dd79065eacd229c1eb5c
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2024-02-10 13:57:09 +0700

    modif homepage dan main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:discuss_app/config/app_color.dart';
+import 'package:discuss_app/config/app_route.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,14 +12,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
           primaryColor: AppColor.primary,
           colorScheme: const ColorScheme.light().copyWith(
             primary: AppColor.primary,
           )),
-      home: Scaffold(),
+      routerConfig: AppRoute.routerConfig,
     );
   }
 }
---
+++ lib/page/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:d_view/d_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+      appBar: DView.appBarCenter('home'),
+    );
+  }
+}
